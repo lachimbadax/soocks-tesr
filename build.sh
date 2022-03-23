61b167465b2e1cb7605395e52fd21bbd2ce058e1
TxThinking <cloud@txthinking.com>
2022-03-23 14:43:32 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,10 +7,11 @@ fi
 
 mkdir _
 
-deno compile -A -r --unstable --target x86_64-unknown-linux-gnu -o _/testsocks5_linux_amd64 https://raw.githubusercontent.com/brook-community/testsocks5/master/testsocks5.js
-deno compile -A -r --unstable --target x86_64-apple-darwin -o _/testsocks5_darwin_amd64 https://raw.githubusercontent.com/brook-community/testsocks5/master/testsocks5.js
-deno compile -A -r --unstable --target x86_64-pc-windows-msvc -o _/testsocks5_windows_amd64 https://raw.githubusercontent.com/brook-community/testsocks5/master/testsocks5.js
+deno compile -A -r --unstable --target x86_64-unknown-linux-gnu -o _/testsocks5_linux_amd64 https://raw.githubusercontent.com/txthinking/testsocks5/master/testsocks5.js
+deno compile -A -r --unstable --target x86_64-apple-darwin -o _/testsocks5_darwin_amd64 https://raw.githubusercontent.com/txthinking/testsocks5/master/testsocks5.js
+deno compile -A -r --unstable --target aarch64-apple-darwin -o _/testsocks5_darwin_arm64 https://raw.githubusercontent.com/txthinking/testsocks5/master/testsocks5.js
+deno compile -A -r --unstable --target x86_64-pc-windows-msvc -o _/testsocks5_windows_amd64.exe https://raw.githubusercontent.com/txthinking/testsocks5/master/testsocks5.js
 
-nami release github.com/brook-community/testsocks5 $1 _
+nami release github.com/txthinking/testsocks5 $1 _
 
 rm -rf _
