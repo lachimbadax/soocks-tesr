60b8a063244609a070665ec15fca20f76049a33f
txthinking <cloud@txthinking.com>
2022-03-28 07:19:04 +0800

    bug fix #1

--- testsocks5.js
+++ testsocks5.js
@@ -7,41 +7,42 @@ import { concat } from "https://deno.land/std@0.88.0/bytes/mod.ts";
 
 var args = parse(Deno.args);
 
-if(args.h || args.help || args.v || args.version || !args.s){
+if (args.h || args.help || args.v || args.version || !args.s) {
     console.log("$ testsocks5 -s SOCKS5_HOST:SOCKS5_PORT");
     console.log("$ testsocks5 -s SOCKS5_HOST:SOCKS5_PORT -u USERNAME -p PASSWORD");
+    console.log("v20220407");
     Deno.exit(0);
 }
 
 var l = args.s.split(":");
-if(l.length != 2 || isNaN(new Number(l[1]))){
+if (l.length != 2 || isNaN(new Number(l[1]))) {
     console.log("Invalid socks5 server");
     Deno.exit(0);
 }
 var host = l[0];
-var port = new Number(l[1]);
+var port = parseInt(l[1]);
 
-try{
-    console.log("Info:\t", "Testing TCP")
+try {
+    console.log("Info:\t", "Testing TCP");
     var c = await Deno.connect({ hostname: host, port: port, transport: "tcp" });
     var r = await new BufReader(c);
     // For convenience, we create a new buffer every time
     var b = new Uint8Array([0x05, 0x01, 0x00]);
-    if(args.u && args.p){
+    if (args.u && args.p) {
         b[2] = 0x02;
     }
     await c.write(b);
     var b1 = new Uint8Array(2);
     await r.readFull(b1);
-    if (b1[0] != 0x05){
+    if (b1[0] != 0x05) {
         console.log("Error:\t", "server is not socks version 5");
         Deno.exit(1);
     }
-    if (b1[1] != b[2]){
+    if (b1[1] != b[2]) {
         console.log("Error:\t", "server does not support method", b[2]);
         Deno.exit(1);
     }
-    if(b[2] == 0x02){
+    if (b[2] == 0x02) {
         b = new Uint8Array([0x01]);
         var u = new TextEncoder().encode(args.u);
         b = concat(b, new Uint8Array([u.length]));
@@ -52,7 +53,7 @@ try{
         await c.write(b);
         b = new Uint8Array(2);
         await r.readFull(b);
-        if (b[1] != 0x00){
+        if (b[1] != 0x00) {
             console.log("Error:\t", "invalid username or password");
             Deno.exit(1);
         }
@@ -61,22 +62,22 @@ try{
     await c.write(b);
     b = new Uint8Array(4);
     await r.readFull(b);
-    if(b[1] != 0x00){
+    if (b[1] != 0x00) {
         console.log("Error:\t", "Rep is not success");
         Deno.exit(1);
     }
-    if(b[3] == 0x01){
+    if (b[3] == 0x01) {
         var b1 = new Uint8Array(6);
         await r.readFull(b1);
     }
-    if(b[3] == 0x04){
+    if (b[3] == 0x04) {
         console.log("Error:\t", "This script does not support IPv6");
         Deno.exit(1);
     }
-    if(b[3] == 0x03){
+    if (b[3] == 0x03) {
         var b1 = new Uint8Array(1);
         await r.readFull(b1);
-        b1 = new Uint8Array(b1[0]+2);
+        b1 = new Uint8Array(b1[0] + 2);
         await r.readFull(b1);
     }
     b = hexdecode("00200001010000010000000000000a74787468696e6b696e6703636f6d0000010001");
@@ -84,36 +85,36 @@ try{
     b = new Uint8Array(65507);
     var i = await r.read(b);
     c.close();
-    if(equals(b.slice(i-4, i), new Uint8Array([0x68, 0xc7, 0x8b, 0x17]))){
+    if (equals(b.slice(i - 4, i), new Uint8Array([0x68, 0xc7, 0x8b, 0x17]))) {
         console.log("OK:\t", "TCP response is OK");
-    }else{
+    } else {
         console.log("Warning", "TCP response is not expected");
     }
-}catch(e){
-    console.log("Error:\t", e)
+} catch (e) {
+    console.log("Error:\t", e);
     Deno.exit(1);
 }
 
-try{
-    console.log("Info:\t", "Testing UDP")
+try {
+    console.log("Info:\t", "Testing UDP");
     var c = await Deno.connect({ hostname: host, port: port, transport: "tcp" });
     var r = await new BufReader(c);
     var b = new Uint8Array([0x05, 0x01, 0x00]);
-    if(args.u && args.p){
+    if (args.u && args.p) {
         b[2] = 0x02;
     }
     await c.write(b);
     var b1 = new Uint8Array(2);
     await r.readFull(b1);
-    if (b1[0] != 0x05){
+    if (b1[0] != 0x05) {
         console.log("Error:\t", "server is not socks version 5");
         Deno.exit(1);
     }
-    if (b1[1] != b[2]){
+    if (b1[1] != b[2]) {
         console.log("Error:\t", "server does not support method", b[2]);
         Deno.exit(1);
     }
-    if(b[2] == 0x02){
+    if (b[2] == 0x02) {
         b = new Uint8Array([0x01]);
         var u = new TextEncoder().encode(args.u);
         b = concat(b, new Uint8Array([u.length]));
@@ -124,7 +125,7 @@ try{
         await c.write(b);
         b = new Uint8Array(2);
         await r.readFull(b);
-        if (b[1] != 0x00){
+        if (b[1] != 0x00) {
             console.log("Error:\t", "invalid username or password");
             Deno.exit(1);
         }
@@ -133,24 +134,24 @@ try{
     await c.write(b);
     b = new Uint8Array(4);
     await r.readFull(b);
-    if(b[1] != 0x00){
+    if (b[1] != 0x00) {
         console.log("Error:\t", "Rep is not success");
         Deno.exit(1);
     }
     var h, p;
-    if(b[3] == 0x01){
+    if (b[3] == 0x01) {
         var b1 = new Uint8Array(4);
         await r.readFull(b1);
         h = b1.join(".");
         b1 = new Uint8Array(2);
         await r.readFull(b1);
-        p = varnum(b1, {dataType: "uint16", endian: "big"});
+        p = varnum(b1, { dataType: "uint16", endian: "big" });
     }
-    if(b[3] == 0x04){
+    if (b[3] == 0x04) {
         console.log("Error:\t", "This script does not support IPv6");
         Deno.exit(1);
     }
-    if(b[3] == 0x03){
+    if (b[3] == 0x03) {
         var b1 = new Uint8Array(1);
         await r.readFull(b1);
         b1 = new Uint8Array(b1[0]);
@@ -158,20 +159,20 @@ try{
         h = new TextDecoder("utf-8").decode(b1);
         b1 = new Uint8Array(2);
         await r.readFull(b1);
-        p = varnum(b1, {dataType: "uint16", endian: "big"});
+        p = varnum(b1, { dataType: "uint16", endian: "big" });
     }
-    var c1 = Deno.listenDatagram({hostname: c.localAddr.hostname, port: c.localAddr.port, transport: "udp"});
+    var c1 = Deno.listenDatagram({ hostname: c.localAddr.hostname, port: c.localAddr.port, transport: "udp" });
     b = hexdecode("000000010808080800350001010000010000000000000a74787468696e6b696e6703636f6d0000010001");
-    await c1.send(b, {transport: "udp", hostname: h, port: p});
+    await c1.send(b, { transport: "udp", hostname: h, port: p });
     b = new Uint8Array(65507);
     var l = await c1.receive(b);
     c1.close();
     c.close();
-    if(equals(l[0].slice(-4), new Uint8Array([0x68, 0xc7, 0x8b, 0x17]))){
+    if (equals(l[0].slice(-4), new Uint8Array([0x68, 0xc7, 0x8b, 0x17]))) {
         console.log("OK:\t", "UDP response is OK");
-    }else{
+    } else {
         console.log("Warning", "UDP response is not expected");
     }
-}catch(e){
-    console.log("Error:\t", e)
+} catch (e) {
+    console.log("Error:\t", e);
 }
